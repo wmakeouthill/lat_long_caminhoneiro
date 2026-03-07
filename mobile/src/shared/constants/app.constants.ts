163229ee4182a83945670c2b3abe4a572501872e
wmakeouthill <wcacorreia1995@gmail.com>
2026-03-07 03:19:20 -0300

    att

--- mobile/src/shared/constants/app.constants.ts
+++ mobile/src/shared/constants/app.constants.ts
@@ -1,7 +1,11 @@
 import Constants from 'expo-constants';
 
 export const API_URL: string =
-  Constants.expoConfig?.extra?.apiUrl ?? 'http://localhost:8000';
+  Constants.expoConfig?.extra?.apiUrl ??
+  (Constants.manifest as any)?.extra?.apiUrl ??
+  'http://SEU_IP_VPS';
+
+console.log('[Config] API_URL:', API_URL);
 
 export const GOOGLE_WEB_CLIENT_ID: string =
   Constants.expoConfig?.extra?.googleWebClientId ?? '';
